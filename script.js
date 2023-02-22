b3de3f5ad7216b3d75ff1700f66eefef4d5a6443
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-02-22 21:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+var url = document.getElementById("url").textContent;
+
+function sharableURL() {
+	var nameElement = document.getElementById("name").value;
+	var yearElement = document.getElementById("year").value;
+	if(nameElement&&yearElement){
+		url=url+"?name="+nameElement+"&year="+yearElement;
+		document.getElementById("url").textContent = url;
+	}
+	else if(nameElement){
+		url=url+"?name="+nameElement;
+		document.getElementById("url").textContent = url;
+	}
+	else if(yearElement){
+		url=url+"?year="+yearElement;
+		document.getElementById("url").textContent = url;
+	}
+}
+
+document.getElementById("submit").addEventListener("click",sharableURL);
