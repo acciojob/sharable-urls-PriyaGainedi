34454bc7a886b5a356a171feb39a0e2ddb4f41fa
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-06-07 10:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ var url = document.getElementById("url").textContent;
 function sharableURL() {
 	var nameElement = document.getElementById("name").value;
 	var yearElement = document.getElementById("year").value;
-	if(nameElement&&yearElement){
+	if(nameElement.trim()&&yearElement){
 		url="https://localhost:8080/?name="+nameElement+"&year="+yearElement;
 		document.getElementById("url").textContent = url;
 	}
-	else if(nameElement&&!yearElement){
+	else if(nameElement.trim()&&!yearElement){
 		url="https://localhost:8080/?name="+nameElement;
 		document.getElementById("url").textContent = url;
 	}
-	else if(yearElement&&!nameElement){
+	else if(yearElement&&!nameElement.trim()){
 		url="https://localhost:8080/?year="+yearElement;
 		document.getElementById("url").textContent = url;
 	}
@@ -22,4 +22,4 @@ function sharableURL() {
 	}
 }
 
-document.getElementById("submit").addEventListener("click",sharableURL);
+document.getElementById("button").addEventListener("click",sharableURL);
